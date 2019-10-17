d76dfb5d795d7cbbe30bb07e96b679361d04215f
wujisheng <lovezhensheng@163.com>
2019-10-17 13:40:55 +0800

    tttt

--- src/com/wu/Test.java
+++ src/com/wu/Test.java
@@ -4,10 +4,9 @@ import com.tan.TanY;
 public class Test {
 
     public static void main(String[] args) {
-        TanY tanY = new TanY("覃友谅","女");
-        System.out.println("hello world！" + tanY.getName() + "\t" +tanY.getSex());
+        TanY tanY = new TanY("覃友谅", "女");
+        System.out.println("hello world！" + tanY.getName() + "\t" + tanY.getSex());
         tanY.setName("谭有亮");
         System.out.println(tanY.getName());
     }
-
 }
